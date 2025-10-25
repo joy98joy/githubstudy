926cd163be75840f7c69b24fe83b6f7ff21a5ae4
joy98joy <129839798+joy98joy@users.noreply.github.com>
2025-10-25 18:55:40 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+import math
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+
+def hello_world():
+
+    return "Hello, DevOps!"
+
+@app.route('/menu')
+
+def menu():
+
+# 'templates' 폴더 안에 'menu.html' 파일이 있다고 가정
+
+    return render_template('menu.html')
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=8080)
